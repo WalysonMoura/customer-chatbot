566b3d7e8856aa92cd331f303b4043564a978667
{ Walyson } <walysonmoura222@gmail.com>
2023-03-30 14:43:30 -0300

    new

--- src/bot/nlp/index.ts
+++ src/bot/nlp/index.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { NeuralNetwork } from "@nlpjs/neural";
 import { NlpManager, ConversationContext } from "node-nlp";
 
@@ -5,8 +6,8 @@ import { NlpManager, ConversationContext } from "node-nlp";
 import { addDocuments } from "./addDocuments";
 import { addAnswers } from "./addAnswers";
 
-//const corpus1 = path.resolve(__dirname, './corpusProducts/corpus-produto1.json');
-//const corpus2 = path.resolve(__dirname, './corpusProducts/corpus-produto2.json');
+const corpus1 = path.resolve(__dirname, './corpusProducts/corpus-produto1.json');
+const corpus2 = path.resolve(__dirname, './corpusProducts/corpus-produto2.json');
 
 
 const nlpManager = new NlpManager({
@@ -25,8 +26,9 @@ export async function nlpManagerConversation(senderMessage: string) {
   //await addDocuments(nlpManager);
  // await addAnswers(nlpManager);
 
- await nlpManager.addCorpus('./corpusProducts/corpus-produto1.json');
- await nlpManager.addCorpus('./corpusProducts/corpus-produto2.json');
+ await nlpManager.addCorpus(corpus1);
+ await nlpManager.addCorpus(corpus2);
+
  /*
   await nlpManager.loadCorpora([
     {
